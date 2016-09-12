a73cf09f49ba300d802657fb52a2a9947cdb8ba0
Mercy <bluegyd@vip.qq.com>
2016-09-12 22:27:06 +0800

    2016-09-12

---
+++ 50_Pow(x, n).swift
@@ -0,0 +1,51 @@
+
+//
+//==============================
+// Author: Mercy
+// Email:  bluegyd@vip.qq.com
+//==============================
+//
+
+
+// Question: -
+
+/**
+ 
+ Implement pow(x, n).
+
+ */
+
+
+// Solution: - 
+
+// 思路：
+// 对于一个任意一个整数 n , n = (n / 2) * 2 + n % 2
+// 所以 pow(x, n) = pow(x, (n / 2) * 2 + n % 2)
+//               = pow(x, (n / 2) * 2) * pow(x, n % 2)
+//               = pow(pow(x, n  2), 2) * pow(x, n % 2)
+//               = pow(x, n  2) * pow(x, n  2) * pow(x, n % 2)
+// 因为 n % 2 要么为 正负一， 要么为 零，则
+// 令 y = pow(x, n / 2),
+// 当 n 为偶数时，pow(x, n) = y * y
+// 当 n 为奇数时，根据 n 的正负性，pow(x, n) = y * y * x 或者 pow(x, n) = y * y / x
+
+class Solution {
+    func myPow(x: Double, _ n: Int) -> Double {
+    
+        if n == 0 {
+            return 1
+        }
+        
+        var y = myPow(x, n / 2)
+        
+        if n % 2 == 0 {
+            return y * y
+        } else {
+            if n > 0 {
+                return y * y * x
+            } else {
+                return y * y / x
+            }
+        }
+    }
+}
