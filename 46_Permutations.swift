6f3b2ab00272c834b620587512297cb3dd7acbac
Mercy <bluegyd@vip.qq.com>
2016-09-13 10:53:48 +0800

    2016-09-13

---
+++ 46_Permutations.swift
@@ -0,0 +1,66 @@
+
+//
+//==============================
+// Author: Mercy
+// Email:  bluegyd@vip.qq.com
+//==============================
+//
+
+
+// Question: -
+
+/**
+ 
+ Given a collection of distinct numbers, return all possible permutations.
+
+ For example, [1,2,3] have the following permutations:
+
+    [
+        [1,2,3],
+        [1,3,2],
+        [2,1,3],
+        [2,3,1],
+        [3,1,2],
+        [3,2,1]
+    ]
+
+ */
+
+
+// Solution: - 
+
+class Solution {
+    
+    // 全局变量记录结果
+    var result = [[Int]]()
+    
+    func permute(nums: [Int]) -> [[Int]] {
+        
+        dfs([Int](), nums)
+        
+        return result
+    }
+    
+    // 深度优先遍历
+    func dfs(middleResult: [Int], _ nums: [Int]) {
+        
+        for i in 0 ..< nums.count {
+            
+            var middleRe = middleResult
+            var tempNums = nums
+            
+            middleRe.append(nums[i])
+            tempNums.removeAtIndex(i)
+            
+            // 递归到最下面一层时，返回
+            if tempNums.count == 0 {
+                result.append(middleRe)
+                return
+            } else {
+                dfs(middleRe, tempNums)
+            }
+            
+        }
+        
+    }
+}
---
+++ 47_PermutationsII.swift
@@ -0,0 +1,67 @@
+
+//
+//==============================
+// Author: Mercy
+// Email:  bluegyd@vip.qq.com
+//==============================
+//
+
+
+// Question: -
+
+/**
+ 
+ Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+ For example, [1,1,2] have the following unique permutations:
+
+    [
+        [1,1,2],
+        [1,2,1],
+        [2,1,1]
+    ]
+
+ */
+
+
+// Solution: - 
+
+class Solution {
+    
+    var result = [[Int]]()
+    
+    func permuteUnique(nums: [Int]) -> [[Int]] {
+        // 先进行排序，然后执行DFS
+        let n = nums.sort()
+        dfs([Int](), n)
+        
+        return result
+    }
+    
+    func dfs(middleResult: [Int], _ nums: [Int]) {
+        
+        for i in 0 ..< nums.count {
+            
+            var middleRe = middleResult
+            var tempNums = nums
+            
+            middleRe.append(nums[i])
+            tempNums.removeAtIndex(i)
+            
+            if tempNums.count == 0 {
+                result.append(middleRe)
+                return
+            } else {
+                // 跳过重复的数字
+                if (i < nums.count - 1) && (nums[i+1] == nums[i]) {
+                    continue
+                }
+                
+                dfs(middleRe, tempNums)
+            }
+            
+        }
+        
+    }
+}
+
